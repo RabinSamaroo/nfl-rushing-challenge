cb4b46cfcf3d8f35fd79776bf80b38822a5a82dd
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-04-12 10:15:46 -0400

    api call working

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const { knex, TABLE_NAME } = require("./db.js");
 const express = require("express");
+const { query } = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
 app.get("/json", (req, res) => {
-  knex(TABLE_NAME)
+  const { filterField, filterAsc, filterPlayer } = req.query;
+  let query = knex(TABLE_NAME)
     .select()
-    .orderBy("Lng", "desc")
+    .orderBy(filterField, filterAsc == "Ascending" ? "asc" : "desc");
+
+  if (filterPlayer) query.where("Player", "like", `%${filterPlayer}%`);
+
+  query
     .then((result) => {
       res.json(result);
     })
