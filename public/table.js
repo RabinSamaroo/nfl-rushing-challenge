3be4a19d57f92cf47c80115877a7aa6a7414473d
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-04-13 10:26:17 -0400

    added jsdoc comments to table.js

--- public/table.js
+++ public/table.js
@@ -1,9 +1,14 @@
+// Grab interactable fields
 const filterField = document.getElementById("filter-field");
 const filterAsc = document.getElementById("filter-asc");
 const filterPlayer = document.getElementById("filter-player");
 const downloadBtn = document.getElementById("download-csv");
 const filterSubmitBtn = document.getElementById("filter-submit");
 
+/**
+ * Takes values provided by the filter section and generates URL safe parameters for the api call
+ * @returns {String} URL Safe parameters
+ */
 function generateParams() {
   const fields = {
     filterField: filterField.value,
@@ -16,6 +21,9 @@ function generateParams() {
     .join("&");
 }
 
+/**
+ * Generates the URL for the CSV download and opens a new tab, initating the download
+ */
 function downloadCSV() {
   const urlParameters = generateParams();
   const url = window.location.origin + "/csv?" + urlParameters;
@@ -23,6 +31,10 @@ function downloadCSV() {
   window.open(url);
 }
 
+/**
+ * Generates the URL for the database and results are returned in json
+ * Calls generate table with the API response
+ */
 function queryDatabase() {
   const urlParameters = generateParams();
   const url = window.location.origin + "/json?" + urlParameters;
@@ -34,6 +46,10 @@ function queryDatabase() {
     });
 }
 
+/**
+ *  Clears the current table and generates a new one based on the input object
+ * @param {Array<Object>} data Object data to be turned into a table
+ */
 function generateTable(data) {
   let table = document.querySelector("table");
   table.innerHTML = "";
@@ -42,6 +58,7 @@ function generateTable(data) {
   let tbody = table.createTBody();
   tbody.classList = "flex-1 sm:flex-none";
 
+  // A new row is added in HEAD to handle extra-small devices responsivly
   for (let i in data) {
     let row_head = thead.insertRow();
     row_head.classList =
@@ -50,7 +67,8 @@ function generateTable(data) {
     let row_body = tbody.insertRow();
     row_body.classList =
       "hover:bg-gray-700 flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0";
-    if (i % 2 === 1) row_body.classList.add("bg-gray-800");
+
+    if (i % 2 === 1) row_body.classList.add("bg-gray-800"); // Handles odd row colouring
 
     for (let key in data[0]) {
       let th = document.createElement("th");
@@ -66,5 +84,6 @@ function generateTable(data) {
   }
 }
 
+// Assign functions to buttons
 filterSubmitBtn.addEventListener("click", queryDatabase);
 downloadBtn.addEventListener("click", downloadCSV);
